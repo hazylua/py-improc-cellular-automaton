3346c802b12f3d62cd34fc38e59f363df62111c1
yb00 <yagobraz98@gmail.com>
2021-03-18 22:22:07 -0300

    Rename

--- image_operations.py
+++ image_operations.py
@@ -4,10 +4,10 @@ from matplotlib import pyplot as plt
 
 img_path = './white_cat.jpg'
 
-def bitmap():
+def read():
     img = cv.imread(img_path)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    binary = cv.threshold(img,127,255,cv.THRESH_BINARY)
+    binary = cv.threshold(img, 120, 255, cv.THRESH_BINARY)
     return binary
 
 def save(img, path):
