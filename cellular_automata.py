7de22c5850e39bb3428a063236334ab4844089e4
yb00 <yagobraz98@gmail.com>
2021-04-01 23:36:01 -0300

    Update to check for local threshold

--- cellular_automata.py
+++ cellular_automata.py
@@ -21,11 +21,14 @@ class CellularAutomata:
     def tickAlgorithm(self):
         # deepcopy?
         field2 = self.field
-        for y in range(0, self.maxY):
-            for x in range(0, self.maxX):
-                neighbours = tuple(self.neighbours(x, y))
+        for y in range(1, self.maxY - 1):
+            for x in range(1, self.maxX - 1):
+                neighbours = list(self.neighbours(x, y))
+                th = neighbours.pop()
+                # print(th, neighbours)
                 if neighbours == self.rule:
-                    field2[x][y] = 255
+                    field2[x][y] = th
+                    # print('yes')
                     continue
                 else:
                     #field2[x][y] 
@@ -36,23 +39,28 @@ class CellularAutomata:
     def neighbours(self, x, y):
         rows = self.maxX
         cols = self.maxY if rows else 0
+        local = 0
         for i in range(max(0, x - 1), min(rows, x + 2)):
             for j in range(max(0, y - 1), min(cols, y + 2)):
                 if i == x and j == y:
                     pass
                 else:
+                    local += self.field[x][y]
                     if self.field[x][y] == self.field[i][j]:
-                        yield 'c'
+                        yield 2
                     elif self.field[x][y] > self.field[i][j]:
                         yield 0
                     else:
-                        yield 255
+                        yield 1
+        th = int(local/8)
+        yield th
+                    
 
     def run(self):
         self.tick()
 
 img = improc.read_preprocess(samples_path + 'white_cat.jpg')
-ca = CellularAutomata(img, (0, 0, 0, 0, 0, 0, 0 ,0))
+ca = CellularAutomata(img, [1, 1, 1, 1, 2, 1, 2 , 2])
 
 ca.run()
 improc.save_img(results_path, 'test.jpg', ca.field)
