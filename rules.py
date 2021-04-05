d8175d27dff101d61f7aef7b362a3abefe5a91f1
yb00 <yagobraz98@gmail.com>
2021-04-04 23:45:29 -0300

    Fix idea

--- rules.py
+++ rules.py
@@ -1,23 +1,25 @@
 """
-Consider simmetries: Apply rule even if pattern is rotated.
-Apply Burnside's lemma to find all possible rules after eliminating symmetries and reflections. for 3 states and neighbors of 8 -> ( 3^8 + 4*3^5 + 2*3^2 + 3^4 ) / 8 = 954 possible patterns.
+Eliminate all symmetries and reflection.
+Burnside's lemma = ( 3^8 + 4*3^5 + 2*3^2 + 3^4 ) / 8 = 954 possible patterns.
 """
 
 import itertools as it
+from numpy import tril, triu, array_equal
 
+# needs to be implemented
 def generate():
-    """ Generate patterns with diagonal simmetries. """   
-    p = list(it.product([0, 1, 2], repeat=3))
-    m = list(it.product([0, 1, 2], repeat=5))
-    results = []
-    for x in p:
-        for y in m:
-            row1 = [y[0], y[1], y[2]]
-            row2 = [x[0], 9, y[3]]
-            row3 = [x[1], x[2], y[4]]
-            matrix = [row1, row2, row3]
-            results.append(matrix)
-    print(results[-1])
+    """ Generate all possible neighborhood configurations. """
+    patterns = list(it.product([0, 1, 2], repeat=8))
+    temp = patterns
+    i = 0
+    while i < len(patterns):
+        results.append(patterns[i])
+        for j in range(i + 1, len(patterns) - 1):
+            # check for simmetry and reflection. idea:
+            # if patterns[j] is simmetry or reflection of patterns[i] then remove pattern of patterns
+        # update pattern length
+        # check next pattern
+    
 
 if __name__ == '__main__':
     generate()
