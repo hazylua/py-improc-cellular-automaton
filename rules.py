a8c92d1f2eef0b91268cf2c70a8ff5f19624e56c
yb00 <yagobraz98@gmail.com>
2021-04-11 19:10:48 -0300

    Store rules

--- rules.py
+++ rules.py
@@ -9,7 +9,8 @@ If it is, remove said simmetric/reflection matrix
 """
 
 import itertools as it
-# import csv
+import csv
+import json
 from matrix import rotate_90_degree_clckwise, x_symmetry, y_symmetry, diagl_symmetry, diagr_symmetry, show_rows, get_matrix, get_pattern, rotate_180_degree_clckwise, rotate_270_degree_clckwise, identity
 
 
@@ -55,8 +56,30 @@ def get_rules(arr, s):
                     break
                 j += 1
         i += 1
-    
+
     return symmetries
 
-# a = get_rules([0, 1, 2], 8)
-# print(a)
+
+if __name__ == "__main__":
+    states = []
+    neighbourhood_size = 0
+
+    with open("./settings.json") as f:
+        settings = json.load(f)
+        states = settings['states']
+        neighbourhood_size = settings['neighbourhood_size']
+
+    rules = get_rules(states, neighbourhood_size)
+    # print(len(rules))
+
+    with open("rules.csv", "w+", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rules)
+
+    # with open("rules.csv", "r") as f:
+    #     reader = csv.reader(f)
+    #     arr = []
+    #     for row in reader:
+    #         print(row)
+    #         arr.append(row)
+    #     print(len(arr))
