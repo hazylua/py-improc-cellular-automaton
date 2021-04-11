9f95799d583f7b7ffd9553b59799d9b377de53e0
yb00 <yagobraz98@gmail.com>
2021-04-11 19:11:32 -0300

    Add helpers

--- main.py
+++ main.py
@@ -5,22 +5,42 @@ Execute.
 from cellular_automata import CellularAutomata
 import image_processing as improc
 from rules import get_rules
+import csv
 
 RESULTS_PATH = "./results/"
 SAMPLES_PATH = "./samples/"
 
+
+def load_image(fpath):
+    im = improc.read_preprocess(SAMPLES_PATH + fpath)
+    return im
+
+
+def load_rules(rpath):
+    arr = []
+    with open(rpath, "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            arr.append(row)
+    return arr
+
+
 if __name__ == "__main__":
     imfile = "white_cat-noise.jpg"
-    
-    arr = [0, 1, 2]
-    neighborhood = 8
-    rules = get_rules(arr, neighborhood)
-    
-    img = improc.read_preprocess(SAMPLES_PATH + imfile)
-    for i, rule in enumerate(rules):
-        pattern = list(rule[0])
-        ca = CellularAutomata(img, pattern)
-        for _ in range(0, 10):
-            ca.run()
-        imresult = f"test_{i}.jpg"
-        improc.save_img(RESULTS_PATH, imresult, ca.field)
+    img = load_image(imfile)
+    rfile = "rules.csv"
+    rules = load_rules(rfile)
+
+    pattern = list(rules[124])
+    ca = CellularAutomata(img, pattern)
+    ca.run()
+    imresult = f"test.jpg"
+    improc.save_img(RESULTS_PATH, imresult, ca.field)
+
+    # for i, rule in enumerate(rules):
+    #     pattern = list(rule[0])
+    #     ca = CellularAutomata(img, pattern)
+    #     for _ in range(0, 10):
+    #         ca.run()
+    #     imresult = f"test_{i}.jpg"
+    #     improc.save_img(RESULTS_PATH, imresult, ca.field)
