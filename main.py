690f42e75e30a4d0c9a7a6c0d36bdb7b1dc35de4
yb00 <yagobraz98@gmail.com>
2021-04-13 23:39:22 -0300

    Update main formatting

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ SAMPLES_PATH = "./samples/"
 def load_image(fpath):
     """ Load image and preprocess. """
     im = improc.read_preprocess(
-        SAMPLES_PATH + fpath, resize=True, height_resize=0.7)
+        SAMPLES_PATH + fpath, resize=True, height_resize=0.2)
     return im
 
 
@@ -32,17 +32,11 @@ if __name__ == "__main__":
     rfile = "rules.json"
     rules = load_rules(rfile)
 
-    pattern = rules[120][0]
-    ca = CellularAutomata(img, pattern)
-    for _ in range(0, 20):
-        ca.run()
-    imresult = f"test.jpg"
-    improc.save_img(RESULTS_PATH, imresult, ca.field)
-
-    # for i, rule in enumerate(rules):
-    #     pattern = rule[0]
-    #     ca = CellularAutomata(img, pattern)
-    #     for _ in range(0, 10):
-    #         ca.run()
-    #     imresult = f"test_{i}.jpg"
-    #     improc.save_img(RESULTS_PATH, imresult, ca.field)
+    for i, rule in enumerate(rules):
+        pattern = rule[0]
+        ca = CellularAutomata(img, pattern)
+        for j in range(0, 20):
+            print(f"{i}:{j} - RULE: {pattern} - Running...")
+            ca.run()
+        imresult = f"result_{i}.jpg"
+        improc.save_img(RESULTS_PATH, imresult, ca.field)
