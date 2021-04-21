cbf1caabceb5efdb111f780bf05fff65bbac831d
yb00 <yagobraz98@gmail.com>
2021-04-20 22:08:46 -0300

    Clear

--- cython/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name="Cellular Automata",
-    ext_modules = cythonize("cellular_automata.pyx"),
-    zip_safe=False
-)
