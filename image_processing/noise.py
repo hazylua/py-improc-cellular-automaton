87ad0cf2df57ef24bc3cc7fc625ecd7e29deab16
yb00 <yagobraz98@gmail.com>
2021-06-08 23:46:17 -0300

    Update noise.py

--- image_processing/noise.py
+++ image_processing/noise.py
@@ -1,32 +1,30 @@
-import random
-import numpy as np
+""" Add noise to grayscale images. """
 
-def add_noise(image, noise, rate=0.005):
+import numpy as np
+from scipy import ndimage
+
+
+def add_noise(image, noise, rate=0.05):
     """ Adds noise to image. """
 
     if noise == "gaussian":
         row, col = image.shape
-        mean = 0
-        var = col
-        sigma = var**0.5
-        gauss = np.random.normal(mean, sigma, (row, col))
-        # gauss = gauss.reshape(row, col)
+        var = ndimage.laplace(image).var()
+        sigma = (var*rate) ** 0.5
+        print(var, sigma)
+        gauss = np.random.normal(loc=0, scale=sigma, size=(row, col)) * rate
         noisy = image + gauss
+        # noisy = image + gauss
         return noisy
 
     elif noise == "salt_pepper":
-        prob = rate
-        output = np.zeros(image.shape,np.uint8)
-        thres = 1 - prob 
-        for i in range(image.shape[0]):
-            for j in range(image.shape[1]):
-                rdn = random.random()
-                if rdn < prob:
-                    output[i][j] = 0
-                elif rdn > thres:
-                    output[i][j] = 255
-                else:
-                    output[i][j] = image[i][j]
+        output = image.copy()
+        black = 0
+        white = 255
+        probs = np.random.random(image.shape[:2])
+        output[probs < (rate / 2)] = black
+        output[probs > 1 - (rate / 2)] = white
+
         return output
 
     else:
