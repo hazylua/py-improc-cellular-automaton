68090eb0cf9f4381c69cfdcedd31a76f3b99a56f
yb00 <yagobraz98@gmail.com>
2021-06-09 23:08:26 -0300

    Update main files

--- logger.py
+++
@@ -1,9 +0,0 @@
-""" Helper logging script. """
-
-from datetime import datetime as date
-
-def write_to_file(text, log_file):
-    print(text)
-    with open(f"./{log_file}", "a+") as f:
-        timestamp = date.now().strftime("%d/%m/%Y - %H:%M:%S")
-        f.write(f"[{timestamp}] {text}\n")
